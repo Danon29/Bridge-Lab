3595ec3ac3d927326e3128d7e7f2c8b0d1377741
usmanovruslan83@gmail.com <usmanovruslan83@gmail.com>
2021-09-05 22:51:13 +0200

    Добавьте файлы проекта.

---
+++ bridge/Abstraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge
+{
+    class Abstraction
+    {
+        protected IFigure figure;
+
+        public Abstraction(IFigure figure)
+        {
+            this.figure = figure;
+        }
+
+        public virtual string Draw()
+        {
+            return "Фигура " +  figure.DrawFigure() + " нарисована";
+        }
+    }
+}
---
+++ bridge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Artist artist = new Artist();
+
+            Abstraction abstraction;
+
+            abstraction = new Abstraction(new Circle());
+            artist.DoSmth(abstraction);
+
+            abstraction = new Abstraction(new Square());
+            artist.DoSmth(abstraction);
+        }
+    }
+}
